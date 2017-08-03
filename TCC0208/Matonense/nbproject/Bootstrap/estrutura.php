3eafb78855396ff6d161431d72a4ffe4655abdae
zikamemo <pedrohenriquepinheiro77@gmail.com>
2017-08-03 08:37:17 -0300

    Primerio commit

---
+++ TCC0208/Matonense/nbproject/Bootstrap/estrutura.php
@@ -0,0 +1,20 @@
+<html> <!-- inicia código em html-->
+
+	<head> 
+		<title>DESAFIO </title> 
+		<!-- CSS Necessário para usar BOOTSTRAP -->
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+		<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css" />
+	</head> 
+	
+<body> 
+
+<div class="container">
+
+
+</div>
+ 
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+</body>
+</html>
